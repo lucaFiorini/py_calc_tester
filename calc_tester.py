9591b1d448b28aa931659432ea62f5bfd629ae12
lucaFiorini <luca.fio45@gmail.com>
2026-03-04 21:27:41 +0100

    Improved tester

--- calc_tester.py
+++ calc_tester.py
@@ -1,52 +1,100 @@
-from typing import Any
+from typing import Any, assert_never
 
 from .calc_types import cellValue,CellPosition
 from .calc_xml_parser import CalcParser
 from pydantic import BaseModel,Field,PrivateAttr
-
+from bs4 import Tag
 class TestSet(BaseModel):
-  match_values       : list[cellValue]|None  = None
-  is_formula         : bool|None             = None
-  is_bold            : bool|None             = None
-  no_formula_drift   : bool                  = False
-  has_bgcolor        : bool|None             = None
-  match_bgcolor      : str|None              = None
+  match_values               : list[cellValue]|None = None
+  match_solution_values      : bool                 = False
 
-  def execute(self, parser : CalcParser, cells : list[CellPosition]):
-      cell_values = [parser.get_cell_value(cell) for cell in cells]
-      cell_formulas = [parser.get_cell_formula(cell) for cell in cells]
-      bgcolors = [parser.get_bgcolor(cell) for cell in cells]
+  is_formula                 : bool|None            = None
+  no_formula_drift           : bool                 = False
 
-      assert len(cell_values) == len(cells)
+  is_bold                    : bool|None            = None
 
-      if self.match_values is not None:
-        for cell_value,value in zip(cell_values, self.match_values):
-          if cell_value != value:
+  has_bgcolor                : bool|None            = None
+  match_bgcolor              : str |None            = None
+  match_solution_bgcolor     : bool                 = False
+
+  def execute(self, submission_parser : CalcParser, solution_parser: CalcParser, cells : list[CellPosition]):
+    submission_values = [submission_parser.get_cell_value(cell) for cell in cells]
+    solution_values = [solution_parser.get_cell_value(cell) for cell in cells]
+    cell_formulas = [submission_parser.get_cell_formula(cell) for cell in cells]
+    
+    submission_bgcolors = [submission_parser.get_bgcolor(cell) for cell in cells]
+    solution_bgcolors = [solution_parser.get_bgcolor(cell) for cell in cells]
+    
+    raw_submission_cell_data = [submission_parser.get_cell_data(cell) for cell in cells]
+    raw_solution_cell_data = [solution_parser.get_cell_data(cell) for cell in cells]
+
+    assert len(submission_values) == len(cells)
+
+    return (
+      self.handle_match_values(submission_values) and 
+      self.handle_match_solution_values(submission_values,solution_values) and
+
+      self.handle_is_formula(cell_formulas) and 
+      self.handle_no_formula_drift(cell_formulas) and
+
+      self.handle_is_bold(raw_submission_cell_data) and
+
+      self.handle_has_bgcolor(submission_bgcolors) and
+      self.handle_match_bgcolor(submission_bgcolors) and
+      self.handle_match_solution_bgcolor(submission_bgcolors,solution_bgcolors)
+    )
+  
+  def handle_match_values(self,cell_values : list[cellValue|None]) -> bool:
+    if self.match_values is not None:
+      return cell_values == self.match_values
+    return True
+  def handle_match_solution_values(self,submission_values : list[cellValue|None], solution_cells : list[cellValue|None]) -> bool:
+    if self.match_solution_values:
+      return submission_values == solution_cells
+    return True
+
+  def handle_is_formula(self,cell_formulas : list[str|None]) -> bool:
+    if self.is_formula is not None:
+      for formula in cell_formulas:
+        if self.is_formula != (formula is not None):
+          return False
+    return True
+  def handle_no_formula_drift(self,cell_formulas: list[str|None]) -> bool: 
+    if self.no_formula_drift:
+      initial_formula = cell_formulas[0]
+      for cell_formlua in cell_formulas[1:]:
+        if cell_formlua != initial_formula:
+          return False
+    return True
+  
+  def handle_is_bold(self,cell_data : list[Tag|None]) -> bool:
+    if self.is_bold is not None:
+      for cell in cell_data:
+        if cell is None:
+          if self.is_bold:
             return False
+        else: 
+          if self.is_bold != (cell.find('b') is not None):
+            return False
+    return True
           
-      if self.is_formula is not None:
-        for formula in cell_formulas:
-          if self.is_formula != (formula is not None):
-            return False
+  def handle_has_bgcolor(self,submission_bgcolors : list[str|None]) -> bool:
+    if self.has_bgcolor is not None:
+      for color in submission_bgcolors:
+        if self.has_bgcolor != (color is not None):
+          return False
+    return True
+  def handle_match_bgcolor(self,submission_bgcolors : list[str|None]) -> bool:
+    if self.match_bgcolor is not None:
+      for color in submission_bgcolors:
+        if color != self.match_bgcolor:
+          return False
+    return True
+  def handle_match_solution_bgcolor(self, submission_bgcolors : list[str|None], solution_bgcolors : list[str|None]):
+    if self.match_solution_bgcolor:
+      return submission_bgcolors == solution_bgcolors
+    return True
         
-      if self.no_formula_drift:
-        initial_formula = cell_formulas[0]
-        for cell_formlua in cell_formulas[1:]:
-          if cell_formlua != initial_formula:
-            return False
-          
-      if self.has_bgcolor is not None:
-        for color in bgcolors:
-          if self.has_bgcolor != (color is not None):
-            return False
-
-      if self.match_bgcolor is not None:
-        for color in bgcolors:
-          if color != self.match_bgcolor:
-            return False
-          
-      return True
-
 class TestResult(BaseModel):
   test_name : str
   possible_score : int
@@ -78,12 +126,12 @@ class Test(BaseModel):
     
     return super().model_post_init(context)
   
-  def execute(self,parser: CalcParser) -> 'TestResultList':
+  def execute(self,submission_parser: CalcParser, solution_parser : CalcParser) -> 'TestResultList':
     return TestResultList(test_results = [
         TestResult (
           test_name=test_case.name,
           possible_score=test_case.weight,
-          passed=test_case.tests.execute(parser,self._cells)
+          passed=test_case.tests.execute(submission_parser, solution_parser, self._cells)
         )
         for test_case in self.cases
       ]
